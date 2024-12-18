5abf0350a2e2a4892b589c1f853ca52e766b2662
Flow <flow@pop-os.fritz.box>
2024-12-18 02:07:37 +0100

    fix: repair mobile menu animation

--- src/components/TheMenu.vue
+++ src/components/TheMenu.vue
@@ -422,6 +422,10 @@ export default {
             }
           }
 
+          @keyframes menuAnimation {
+            from { transform: translateX(-100%); opacity: 0 }
+            to { transform: translateX(0%); opacity: 1 }
+          }
           @-webkit-keyframes menuAnimation {
             from { transform: translateX(-100%); opacity: 0 }
             to { transform: translateX(0%); opacity: 1 }
